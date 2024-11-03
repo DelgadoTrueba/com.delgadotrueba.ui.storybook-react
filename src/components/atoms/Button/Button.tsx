62598d57465db96ad8674b786a692eba24c8e38c
Delgado Trueba <jose.delgado@babel.es>
2024-11-03 21:37:14 +0100

    feat: add react II

--- src/components/atoms/Button/Button.tsx
+++ src/components/atoms/Button/Button.tsx
@@ -1,7 +1,18 @@
+import { useState } from 'react';
 import { ButtonProps } from './Button.types';
 
 const Button: React.FC<ButtonProps> = ({ label }) => {
-  return <button type="button">{label}</button>;
+  const [name, setName] = useState<string>(label);
+
+  const handleClick = () => {
+    setName('1');
+  };
+
+  return (
+    <button type="button" onClick={handleClick}>
+      {name}
+    </button>
+  );
 };
 
 export { Button };
